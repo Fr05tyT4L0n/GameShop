d9977462ff0bd62b4777627a7014ad08f4b1e958
Fr05tyT4L0n <takdanai.duangporn@e-tech.ac.th>
2025-01-30 17:08:40 +0700

    change color theme

--- GameShop/obj/Debug/net8.0/GameShop.AssemblyInfo.cs
+++ GameShop/obj/Debug/net8.0/GameShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a68b9dd4650e867825e83f2bd31cb89cfae156d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+020162c39183bf6d4e52f27a8bf91a64e8719781")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
